ccb1f9027a01a6e4fe996895cd96ff9aeaba7aeb
petermirithu <pyra_m.k@yahoo.com>
2019-10-25 22:10:00 +0300

    create a constructor with quotes in quotes component

--- src/app/quote-class.ts
+++ src/app/quote-class.ts
@@ -1,5 +1,9 @@
 export class QuoteClass {
-  author: string;
-  quote: string;
-  submitter: string;
+  
+  constructor(public author: string,public quote: string, public submitter: string){
+
+  }
+  
+  
+  
 }
--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {QuoteClass} from '../quote-class';
 
 @Component({
   selector: 'app-quotes',
@@ -7,6 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuotesComponent implements OnInit {
 
+quotes:QuoteClass[] = [
+  new QuoteClass("Why The Lucky Stiff", "when you dont create things you become defined by your tastes rather than ability. your tastes only narrow & exclude people. so create", "Pyra"),
+  new QuoteClass("Linus Torvalds", "Talk is cheap. Show me the code.","Jedidah"),
+  new QuoteClass("Harold Abelson","Programs must be written for people to read, and only incidentally for machines to execute.","Bernad"),
+]
   constructor() { }
 
   ngOnInit() {
