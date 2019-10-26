0604d3439e9648a78495f6c2e9b42e32917b428c
petermirithu <pyra_m.k@yahoo.com>
2019-10-26 17:35:44 +0300

    add upVote and downVote property in quote class.

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,6 +1,6 @@
 export class Quote {
   showSubmitter : boolean;
-  constructor(public author:string, public quoteIt:string,public submitter:string,public createdDate: Date){
+  constructor(public author:string, public quoteIt:string,public submitter:string,public createdDate: Date,public upVote: number, public downVote:number){
     this.showSubmitter=false;
   }
 }
--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit} from '@angular/core';
-import {Quote} from '../quote'
+import { Component, OnInit,Output} from '@angular/core';
+import {Quote} from '../quote';
 
 @Component({
   selector: 'app-quotes',
@@ -9,9 +9,9 @@ import {Quote} from '../quote'
 export class QuotesComponent implements OnInit {
 
 quotes:Quote[] = [
-  new Quote("Harold Abelson","Programs must be written for people to read, and only incidentally for machines to execute.","Pyra",new Date(2019,9,4)),
-  new Quote("Why The Lucky Stiff", "when you dont create things you become defined by your tastes rather than ability. your tastes only narrow & exclude people. so create.", "Bernad",new Date(2014,0,15)),
-  new Quote("Linus Torvalds", "Talk is cheap. Show me the code.","Jedidah",new Date(1999,8,24)),
+  new Quote("Harold Abelson","Programs must be written for people to read, and only incidentally for machines to execute.","Pyra",new Date(2019,9,4),5,2),
+  new Quote("Why The Lucky Stiff", "when you dont create things you become defined by your tastes rather than ability. your tastes only narrow & exclude people. so create.", "Bernad",new Date(2014,0,15),3,8),
+  new Quote("Linus Torvalds", "Talk is cheap. Show me the code.","Jedidah",new Date(1999,8,24),8,5),  
   
 ]
 
@@ -34,6 +34,10 @@ deleteQuote(isDelete,index){
 //   }
 // }
 
+addNewQuote(quote){
+  this.quotes.push(quote)
+  ;
+}
   constructor() { }
 
   ngOnInit() {
