81f7e139711ea3e9d241f6c8480eb8930ad585e9
petermirithu <pyra_m.k@yahoo.com>
2020-07-31 11:17:58 +0300

    add auth

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {  QuotesComponent } from './quotes/quotes.component'
+import { RegisterComponent } from './register/register.component';
 
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+  { path:'home', component: QuotesComponent},
+  // { path:'auth', component: RegisterComponent},
+  // { path: '',redirectTo:"/home",pathMatch:"full"},  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/date-count.pipe.ts
+++ src/app/date-count.pipe.ts
@@ -9,7 +9,7 @@ export class DateCountPipe implements PipeTransform {
   transform(value: any): number {
     let today:Date = new Date();
     let todayWithNoTime:any = new Date(today.getFullYear(), today.getMonth(), today.getDate())
-    var dateDifference = Math.abs(value-todayWithNoTime)
+    var dateDifference = Math.abs(todayWithNoTime-value)
     var dateDifferenceSeconds =dateDifference*0.001;
     var dateCounter = dateDifferenceSeconds/86400;
 
